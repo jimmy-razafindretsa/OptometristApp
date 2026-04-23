8341690fc6241154f93d6af5265e71180cc24dd0
jimmy-razafindretsa <jimmyyoelrazafindretsa@gmail.com>
2026-04-22 20:36:56 -0400

    feat : Npgsql package to write raw sql in the services

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using OptometristApp.Components;
 
+//builds the app with default configuration 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//allows the razor components to be added gracefully 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+//builds the app into the app varable 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +17,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
